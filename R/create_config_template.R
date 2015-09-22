af45aeef34d7d8706fa8274cd22e226dad2abc63
jlewis91 <jlewis91@users.noreply.github.com>
2015-09-22 13:25:11 +0200

    Create create_config_template.R

---
+++ R/create_config_template.R
@@ -0,0 +1,32 @@
+#' Build config.json template
+#'
+#' \code{auth_gdrive} generates a template config file.
+#'
+#'
+#' @param config_path path where the config.json file is located
+#' @return null
+#'
+#' @examples
+#' \dontrun{
+#' createConfigTemplate()
+#' }
+#' @export
+createConfigTemplate <- function(config_path = ""){
+  txt = '{
+      "google_analytics":{
+        "account_username": "_____",
+        "client_id": "______.apps.googleusercontent.com",
+        "client_secret":"________"
+      }, "google_drive": {
+        "account_username": "____",
+        "client_id": "______.apps.googleusercontent.com",
+        "client_secret":"_____",
+        "query_folder_id":"______"
+      }
+    }'
+  if(!file.exists(paste0(config_path, "config.json"))){
+    cat(txt, file = paste0(config_path, "config.json"))  
+  } else {
+    warning(paste0(config_path, "config.json already exists!"))
+  }  
+}
