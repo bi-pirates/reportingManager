3c25ff18494a01828b9661cb75588c599c3634c4
Jeremiah <jlewis91@users.noreply.github.com>
2016-07-19 17:46:07 +0200

    Update rzrTools_buildReport.R

--- R/rzrTools_buildReport.R
+++ R/rzrTools_buildReport.R
@@ -4,7 +4,8 @@
 #' @param output_file name of the report file on output
 #' @param output_dir directory the report should be written to
 #' @param params parameters to be passed on to the rmarkdown file
-#' @param envir environment which should be used to execute rmarkdown::render; often useful to specify the reporting package, e.g. \code{loadNamespace("FourSeasons")}
+#' @param envir environment which should be used to execute rmarkdown::render;
+#' often useful to specify a new environment including the reporting package, e.g. \code{new.env(parent = loadNamespace("packageName"))}
 #'
 #' @return NULL
 #' @export
