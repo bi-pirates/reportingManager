323ad2c8f9205f94e88b143cb5aa7fc9b7b23bcf
Marco Koch <Marco.Koch@razorfish.de>
2016-07-21 09:15:13 +0200

    update build Report

--- R/rzrTools_buildReport.R
+++ R/rzrTools_buildReport.R
@@ -1,7 +1,7 @@
 #' Build an Rmarkdown report into a PDF
 #'
 #' @param rmarkdown_path path of the rmarkdown file
-#' @param output_file name of the report file on output
+#' @param output_file name of the report file on output, without file suffix
 #' @param output_dir directory the report should be written to
 #' @param params parameters to be passed on to the rmarkdown file
 #' @param envir environment which should be used to execute rmarkdown::render;
@@ -9,17 +9,18 @@
 #'
 #' @return NULL
 #' @export
-buildReport <- function(rmarkdown_path, output_file, output_dir = getwd(), params = list(), envir = new.env()){
+buildReport <- function(rmarkdown_path, output_file = "report", output_dir = getwd(), params = list(), envir = new.env()){
   install_casperjs()
   print("Successfully installed dependencies.")
 
+  htmlPath <- paste0(output_dir, "/", output_file, ".html")
+  pdfPath <- paste0(output_dir, "/", output_file, ".pdf")
+  
   # Render HTML Report
   rmarkdown::render(rmarkdown_path, envir = envir,
-                    output_dir = output_dir, output_file = paste0(output_file,".html"),
+                    output_dir = output_dir, output_file = htmlPath,
                     params = params)
 
   # Render PDF Report
-  htmlPath <- paste0(output_dir, "/", output_file, ".html")
-  pdfPath <- paste0(output_dir, "/", output_file, ".pdf")
   casperHTMLtoPDF(htmlPath, pdfPath)
 }
